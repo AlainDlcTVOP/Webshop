807774470de0abb4aded9c2198709892153306db
jenny <jenny.rigsjo@live.com>
2022-10-27 15:36:59 +0200

    Add ShippedDate

--- Backend/Backend/Models/Order.cs
+++ Backend/Backend/Models/Order.cs
@@ -7,6 +7,7 @@ namespace SkiShop.Models
         public int Id { get; set; }
         public int OrderNr { get; set; }
         public DateTime Date { get; set; }
+        public DateTime ShippedDate { get; set; }
         public DateTime DeliveryDate { get; set; }
         public string Status { get; set; }
         public string Comments { get; set; }
