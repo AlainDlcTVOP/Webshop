9a20100b9f2882a31a38aeccdc6ffb4bc2461033
jenny <jenny.rigsjo@live.com>
2022-10-27 15:51:31 +0200

    Add vertical space for code readability.

--- Backend/Backend/Models/User.cs
+++ Backend/Backend/Models/User.cs
@@ -3,15 +3,25 @@
     public class User
     {
         public int Id { get; set; }
+
         public string UserName { get; set; }
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
         public string Address{ get; set; }
+
         public string PostalCode { get; set; }
+
         public string City { get; set; }
+
         public string Phone { get; set; }
+
         public string Email { get; set; }
+
         public string Password { get; set; }
+
         public bool IsAdmin { get; set; }
     }
 }
