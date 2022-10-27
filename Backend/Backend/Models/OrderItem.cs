beaf9638d561f1501f119851b469f6350be6afaa
jenny <jenny.rigsjo@live.com>
2022-10-27 14:31:43 +0200

    Add controller and model directories/classes

---
+++ Backend/Backend/Models/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿namespace SkiShop.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int OrderNumber { get; set; }
+        public string Product { get; set; }
+        public int Quantity{ get; set; }
+        public double Price { get; set; }
+        public double RowAmount { get; set; }
+
+    }
+}
