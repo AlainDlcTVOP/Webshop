8c16f9dae53f657409d08564cb380ec34c93cd80
jenny <jenny.rigsjo@live.com>
2022-10-28 14:40:50 +0200

    Add list of Images as nav property to Product

--- Backend/Backend/Models/Product.cs
+++ Backend/Backend/Models/Product.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string[] Images { get; set; }
+        public List<Image> Images { get; set; } // Navigation property
         public double Price { get; set; }
         public int InStock { get; set; }
     }
