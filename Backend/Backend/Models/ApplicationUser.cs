227f256e39297300492254a9021da6fe4bedf182
jenny <jenny.rigsjo@live.com>
2022-11-01 14:03:11 +0100

    Remove unnecessary fields/props from ApplicationUser model

--- Backend/Backend/Models/ApplicationUser.cs
+++ Backend/Backend/Models/ApplicationUser.cs
@@ -4,14 +4,14 @@ namespace SkiShop.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string UserName { get; set; }
+        //public string UserName { get; set; }
 
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
-        public string Email { get; set; }
+        //public string Email { get; set; }
 
-        public string Password { get; set; }
+        //public string Password { get; set; }
     }
 }
