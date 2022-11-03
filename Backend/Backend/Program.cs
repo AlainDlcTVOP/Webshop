bd3965c42e2244ae9c05d306d28054546c516cdc
jenny <jenny.rigsjo@live.com>
2022-11-03 09:08:14 +0100

    Add MapControllerRoute to enable api routing.

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -55,6 +55,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
