91b2f67c2c9448ae4b9361281bf5150835b118c3
jenny <jenny.rigsjo@live.com>
2022-11-04 15:48:54 +0100

    Add CRUD methods to ProductsController

--- Backend/Backend/Controllers/ProductsController.cs
+++ Backend/Backend/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using SkiShop.Models;
 using Backend.Models;
 using System.Data;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Backend.Controllers
 {
@@ -18,7 +19,8 @@ namespace Backend.Controllers
             _context = context;
         }
 
-        public IQueryable<ProductDTO> GetAll()
+        [HttpGet]
+        public IQueryable<ProductDTO> GetAllProducts() //api/products
         {
             var products = from p in _context.Products
                          .Include(p => p.Images)
@@ -32,6 +34,105 @@ namespace Backend.Controllers
                                InStock = p.InStock,
                            };
             return products;
+        }
+
+
+        [HttpGet("{id}")] //api/products/3
+        public IActionResult GetProduct(int id)
+        {
+            var product = from p in _context.Products
+                          .Where(p => p.Id == id)
+                          .Include(p => p.Images)
+                            select new ProductDTO()
+                            {
+                                Id = p.Id,
+                                Name = p.Name,
+                                Description = p.Description,
+                                Price = p.Price,
+                                Images = p.Images.Select(i => new ImageDTO { Id = i.Id, ProductID = i.ProductID, Name = i.Name, Src = i.Src, Alt = i.Alt }).ToList(),
+                                InStock = p.InStock,
+                            };
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(product);
+            }
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] CreateProductViewModel model)
+        {
+            if (model == null)
+            {
+                return BadRequest();
+            }
+
+            Product product = new() 
+            {
+                Name = model.Name,
+                Description = model.Description,
+                Price = model.Price,
+                InStock = model.InStock
+            };
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut]
+        public IActionResult UpdateProduct([FromBody] UpdateProductViewModel model)
+        {
+            if (model == null)
+            {
+                return BadRequest();
+            }
+
+            var product = _context.Products.Include(p => p.Images).Where(p => p.Id == model.Id).ToList().First();
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                product.Name = model.Name;
+                product.Description = model.Description;
+                product.Price = model.Price;
+                product.InStock = model.InStock;
+
+                _context.Update(product);
+                _context.SaveChanges();
+
+                return new NoContentResult();
+            }
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+                return NoContent();
+            }
         }
     }
 }
