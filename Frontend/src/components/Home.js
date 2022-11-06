7260434effd57df68f2a0452b27d426193221c9b
Alain <alain_dlc@outlook.com>
2022-11-06 16:28:25 +0100

    banner slider

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -2,11 +2,14 @@ import React from "react";
 import Navigation from "./Navigation";
 import Cool from "./Cool";
 
+import Slidetime from "./Slidetime";
+
 const Home = () => {
   
     return (
          <>
             <Navigation /> 
+            <Slidetime/>
             {/*<Cool/>  if we want it */}    
             <Cool/>
        </>
