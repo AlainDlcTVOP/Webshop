2a2eeb12533fe0bc53177b10983edccad29bb186
jenny <jenny.rigsjo@live.com>
2022-11-12 19:24:32 +0100

    Ajax Orders Controller part 2.

---
+++ Backend/Backend/wwwroot/js/shoppingController.js
@@ -0,0 +1,21 @@
+﻿// Define Javascript functions to call and fetch information from methods in Controllers > ShoppingController.cs
+
+function AddToCart(productID) {
+
+    console.log("AddToCart");
+    console.log(productID);
+
+    $.ajax({
+        type: 'GET',
+        url: `/Shopping/AddToCart?Id=${productID}&Quantity=1`,
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            console.log(response);
+            //let result = document.getElementById("result");
+            //result.innerHTML = response;
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+}
